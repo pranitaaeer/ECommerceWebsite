193b612226a0019885b765c8ae239acf85e9f911
pranitaaeer <aeerpranita@gmail.com>
2025-12-25 10:51:50 +0530

    update changes in usercontroller

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,81 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/user.models.js";
+import { SignupRequestBody } from "../types/types.js";
+import { AsyncHandler } from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
+
+export const newUser = AsyncHandler(
+  async (
+    // {}-> for params ,{}-> for response and third will be for request 
+    // so need to get data from req.body that's why customise it
+    req: Request<{}, {}, SignupRequestBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { name, email, photo, gender, _id, dob } = req.body;
+
+    let user = await User.findById(_id);
+
+    if (user)
+      return res.status(200).json({
+        success: true,
+        message: `Welcome, ${user.name}`,
+      });
+
+    if ([_id, name, email, photo, gender].some((elem) => elem.trim() === "")) {
+      return next(new ErrorHandler("All fields are required", 400));
+    }
+
+    if (!dob) return next(new ErrorHandler("Date of Birth is required", 400));
+
+
+    user = await User.create({
+      name,
+      email,
+      photo,
+      gender,
+      _id,
+      dob: new Date(dob),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: `Welcome, ${user.name}`,
+    });
+  }
+);
+
+export const getAllUsers = AsyncHandler(async (req, res, next) => {
+  const users = await User.find({});
+
+  return res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+export const getUser = AsyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const deleteUser = AsyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+  await user.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
