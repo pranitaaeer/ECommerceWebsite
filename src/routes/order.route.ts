755d4d373a2d43041a2cf745d0921509c4efa7c9
pranitaaeer <aeerpranita@gmail.com>
2025-12-27 10:23:20 +0530

    add order route

---
+++ src/routes/order.route.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { adminOnly } from "../middlewares/auth.js";
+import {
+  allOrders,
+  deleteOrder,
+  getSingleOrder,
+  myOrders,
+  newOrder,
+  processOrder,
+} from "../controllers/order.controllers.js";
+
+const app = express.Router();
+
+// route - /api/v1/order/new
+app.post("/new", newOrder);
+
+// route - /api/v1/order/my
+app.get("/my", myOrders);
+
+// route - /api/v1/order/my
+app.get("/all", adminOnly, allOrders);
+
+app.get("/:orderId",getSingleOrder)
+app.put("/:orderId",adminOnly, processOrder)
+app.delete("/:orderId",adminOnly, deleteOrder);
+
+export default app;
