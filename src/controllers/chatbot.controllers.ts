dbe766a492b32f4ae01329ddc89bfb1cdefa08a1
Pranita Aeer <aeerpranita@gmail.com>
2026-08-31 16:16:33 +0530

    Change model from llama-3.3 to openai/gpt-oss-20b

--- src/controllers/chatbot.controllers.ts
+++ src/controllers/chatbot.controllers.ts
@@ -44,7 +44,7 @@ const GraphState = Annotation.Root({
 
 const model = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
-  model: "llama-3.3-70b-versatile",
+  model: "openai/gpt-oss-20b",
   temperature: 0,
 });
 
